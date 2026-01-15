eca0e86097a606dc4930c11166a75533b8adb5e3
Dell <Dell@DESKTOP-13B42NJ>
2026-01-15 08:59:38 +0530

    Add project files.

---
+++ oopsExample_Staticconstructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsExample_Staticconstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            employee emp1 = new employee(121, "rama", 1000);//this is emp1 object
+            emp1.displayempinfo();
+            employee emp2 = new employee(122, "raju", 2000);//this is emp2 object
+            emp2.displayempinfo();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ oopsExample_Staticconstructor/employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsExample_Staticconstructor
+{
+    internal class employee
+    {
+        //instance variables
+        int eno;
+        string ename;
+        int esal;
+        //static variable
+        static string CompanyName;
+        static employee()//static constructor.
+        {//static constructor is used to assign the data to static variables
+            CompanyName = "Microsoft";
+            //eno = 100;//Don't assign normal variables inside static constructor.it will throw compiletime error.
+        }
+        public employee(int empno, string empname, int empsal)
+        {//non static  constructor is used to assign the data to instance variables
+            eno = empno;
+            ename = empname;
+            esal = empsal;
+           
+        }
+        public void displayempinfo()
+        {
+            Console.WriteLine("emp no is:" + eno);
+            Console.WriteLine("emp name is:" + ename);
+            Console.WriteLine("salry is:" + esal);
+            Console.WriteLine("companny name is:" + CompanyName);
+        }
+    }
+}
